23af2b0cf3f95a23992eff6c128e56d940a1882e
Jessica Temporal <jtemporal@users.noreply.github.com>
2024-02-29 03:21:24 +0000

    Adiciona script da aula 14

---
+++ 14/cria_zip.py
@@ -0,0 +1,14 @@
+import os
+from zipfile import ZipFile
+
+def compactar(pasta, lista_extensoes, caminho_resultado):
+    with ZipFile(caminho_resultado, 'w') as zip_resultante:
+        for raiz, _, arquivos in os.walk(pasta):
+            caminho_relativo = os.path.relpath(raiz, pasta)
+            for arquivo in arquivos:
+                _, ext = os.path.splitext(arquivo)
+                if ext.lower() in lista_extensoes:
+                    zip_resultante.write(
+                        os.path.join(raiz, arquivo),
+                        arcname=os.path.join(caminho_relativo, arquivo)
+                    )
